edce55550e623717b94a5c763c7b8383dc697683
rjain-iitr <rjain.iitr@gmail.com>
2016-06-20 10:31:03 +0530

    adding encryption changes

--- cinder/kims_api.py
+++ cinder/kims_api.py
@@ -1,10 +1,14 @@
+import string
+import random
+from Crypto.Cipher import AES
+from oslo_log import log as logging
+import base64
+import MySQLdb
 
 
+LOG = logging.getLogger(__name__)
 
-
-
-
-MASTER_KEY="MASTER_KEY"
+MASTER_KEY="558MI2LKK86CGAJMBVNNCD3KIPZTA4SY"
 def CreateEncryptedKey(project_id):
        user_key=_get_user_deprecated_key(project_id)
        encrypted_key=_create_new_volume_encrypted_key(user_key)
@@ -24,15 +28,41 @@ def _get_user_deprecated_key(project_id):
        return _decrypte_key(user_key,MASTER_KEY)
        
 def _check_user_encrypted_key(project_id):
-       #TODO Check in DB
-       return None
+       user_key=_check_in_db(project_id)
+       return user_key
 
 def _create_user_encrypted_key(project_id):
        user_key=_create_new_user_key()
        encrypted_key=_encrypt_key(user_key,MASTER_KEY)
-       #TODO save in db
+       _save_in_db(project_id,encrypted_key)
        return encrypted_key
 
+def _check_in_db(project_id):
+       db = MySQLdb.connect("localhost","key_user","kmis_pass","kims" )
+       cursor = db.cursor()
+       sql = "SELECT user_key FROM userkeys WHERE project_id ='%s'" % (project_id)
+       try:
+          cursor.execute(sql)
+          row = cursor.fetchone()
+          if row is not None:
+             return row[0]
+       except:
+           print "Error: unable to fecth data"
+       return None
+
+def _save_in_db(project_id,user_key):
+       db = MySQLdb.connect("localhost","key_user","kmis_pass","kims" )
+       cursor = db.cursor()
+       sql = "INSERT INTO userkeys(project_id, user_key) \
+       VALUES ('%s', '%s' )" % \
+       (project_id, user_key)
+       try:
+          cursor.execute(sql)
+          db.commit()
+       except:
+          db.rollback()
+
+       db.close()
 
 def _create_new_volume_encrypted_key(user_key):
        volume_key=_create_new_volume_key()
@@ -40,19 +70,24 @@ def _create_new_volume_encrypted_key(user_key):
        return encrypted_key
 
 def _create_new_volume_key():
-       #TODO create volume key
-       return "Volume_Key" 
+       key=id_generator(80)
+       return key
 
 def _create_new_user_key():
-       #TODO create user key
-       return "USER_Key"
+       key=id_generator(32)
+       return key
 
 def _encrypt_key(key,encryption_key):
-       #TODO do encryption
-       return key
+       encryption_suite = AES.new(encryption_key, AES.MODE_ECB)
+       cipher_text = base64.b64encode(encryption_suite.encrypt(key))
+       return cipher_text
 
 def _decrypte_key(key,encryption_key):
-       #TODO do decryption
-       return key
-     
+       decryption_suite = AES.new(encryption_key, AES.MODE_ECB)
+       plain_text = decryption_suite.decrypt(base64.b64decode(key))
+       return plain_text
 
+
+ 
+def id_generator(size, chars=string.ascii_uppercase + string.digits):
+   return ''.join(random.choice(chars) for _ in range(size))
