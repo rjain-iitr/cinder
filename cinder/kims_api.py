def42b93877ddc30521e88b888b894999bc212f5
rjain-iitr <rjain.iitr@gmail.com>
2016-06-24 15:23:38 +0530

    handle duplicate with multiple retries

--- cinder/kims_api.py
+++ cinder/kims_api.py
@@ -34,37 +34,60 @@ def _check_user_encrypted_key(project_id):
        return user_key
 
 def _create_user_encrypted_key(project_id):
-       user_key=_create_new_user_key()
-       encrypted_key=_encrypt_key(user_key,MASTER_KEY)
-       _save_in_db(project_id,encrypted_key)
+       val=0
+       count =0
+       retries=100
+       encrypted_key=None
+       while val==0 and count<retries:
+           user_key=_create_new_user_key()
+           encrypted_key=_encrypt_key(user_key,MASTER_KEY)
+           val= _save_in_db(project_id,encrypted_key)
+           count=count+1
+       if val==0 or val==2:
+             raise Exception('Unable to write in db') 
        return encrypted_key
 
 def _check_in_db(project_id):
        db = MySQLdb.connect("localhost","key_user","kmis_pass","kims" )
        cursor = db.cursor()
        sql = "SELECT user_key FROM userkeys WHERE project_id ='%s'" % (project_id)
+       result=None
        try:
           cursor.execute(sql)
-          row = cursor.fetchone()
+          row = cursor.fetchone() 
           if row is not None:
-             return row[0]
-       except:
-           print "Error: unable to fecth data"
-       return None
+             result= row[0]
+       except Exception as e:
+             LOG.error("Unable to fetch from db",e)
+
+       cursor.close()
+       db.close()
+
+       return result
 
 def _save_in_db(project_id,user_key):
        db = MySQLdb.connect("localhost","key_user","kmis_pass","kims" )
        cursor = db.cursor()
+       returnval=1
        sql = "INSERT INTO userkeys(project_id, user_key) \
        VALUES ('%s', '%s' )" % \
        (project_id, user_key)
        try:
           cursor.execute(sql)
           db.commit()
-       except:
+       except MySQLdb.IntegrityError as e:
+          if e.args[0]==1062:
+              returnval=0
+          else:
+              returnval=2
+       except Exception as e:
           db.rollback()
+          returnval=2
+          LOG.error("Unable to write in the db",e)
 
+          
        db.close()
+       return returnval
 
 def _create_new_volume_encrypted_key(user_key):
        volume_key=_create_new_volume_key()
