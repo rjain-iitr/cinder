b257f535bbe0cc46c183684cb6828589b078d2b9
rjain-iitr <rjain.iitr@gmail.com>
2016-06-24 08:09:22 +0530

    changes for pull request

--- cinder/backup/api.py
+++ cinder/backup/api.py
@@ -290,7 +290,7 @@ class API(base.Base):
         volume_type=backup['volume_type_id']
         volume_type_obj=None
         if volume_type is not None:
-                 volume_type_obj=volume_types.get_volume_type(context, volume_type)
+                 volume_type_obj=volume_types.get_volume_type(context, "standard")
         # Create a volume if none specified. If a volume is specified check
         # it is large enough for the backup
         if volume_id is None:
