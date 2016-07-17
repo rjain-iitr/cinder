a4d74ba716869edded2acc46c82e11f527ca613c
rjain-iitr <rjain.iitr@gmail.com>
2016-07-17 16:11:52 +0530

    kims_partial changes

--- cinder/kims_api.py
+++ cinder/kims_api.py
@@ -5,31 +5,24 @@ from oslo_log import log as logging
 import base64
 import MySQLdb
 from cinder import exception
-
+#import cinder.db.sqlalchemy.kims_db as kims_db
 LOG = logging.getLogger(__name__)
 import ConfigParser
-
+from sqlalchemy import DateTime
+from datetime import datetime
 import uuid
 
 
 
-config = ConfigParser.ConfigParser()
-config.read('/etc/cinder/kims.conf')
-
-USER_NAME =config.get('DB', 'USER_NAME',0)
-USER_PASS =config.get('DB', 'USER_PASS',0)
-DATABASE =config.get('DB', 'DATABASE_NAME',0)
-
-
-
+separator="---"
 
 def CreateEncryptedKey(project_id):
-       user_key=_get_user_deprecated_key(project_id)
-       encrypted_key=_create_new_volume_encrypted_key(user_key)
+       cmk_id,version,user_key=_get_user_deprecated_key(project_id)
+       encrypted_key=_create_new_volume_encrypted_key(user_key,cmk_id,version)
        return encrypted_key
 
 def GetPlainTextKey(project_id,encrypted_key):
-       user_key=_check_user_encrypted_key(project_id)
+       user_key=_get_user_encrypted_key_with_encrypted_key(project_id,encrypted_key)
        if user_key is None: 
            raise exception.NotAuthorized()
        #TODO Throw exception
@@ -43,34 +36,44 @@ def _get_user_deprecated_key(project_id):
        MASTER_KEY=_get_master_key()
        if user_key is None:
           user_key=_create_user_encrypted_key(project_id,MASTER_KEY)
-       return _decrypte_key(user_key,MASTER_KEY)
+       yield cmk_id
+       yield version
+       yield _decrypte_key(user_key,MASTER_KEY)
        
 def _check_user_encrypted_key(project_id):
        user_key=_check_in_db(project_id)
+       #user_key=kims_db.check_in_db(project_id)
+       return user_key
+
+def _get_user_encrypted_key_with_encrypted_key(project_id,encrpyted_key):
+       user_key=_check_in_db(project_id)
+       #user_key=kims_db.check_in_db(project_id)
        return user_key
 
 def _get_master_key():
        _key=_check_in_db("MASTER_KEY")
+       #_key=kims_db.check_in_db("MASTER_KEY")
        if _key is None:
           _generated_key=_create_new_master_key()
-          val=_save_in_db("MASTER_KEY",_generated_key)
+          val=_save_in_db("MASTER_KEY",_create_cmk_id(),1,datetime.now(),_generated_key)
+          #val=kims_db.save_in_db("MASTER_KEY",_generated_key)
           if val==1 or val==0:
              _key=_check_in_db("MASTER_KEY")
+             #_key=kims_db.check_in_db("MASTER_KEY")
           elif val==2:
              raise Exception('Unable to create_Master_key') 
        return _key
-
-def _create_new_master_key():
-       return _create_new_user_key()
 
 def _create_user_encrypted_key(project_id,MASTER_KEY):
        val=0
        encrypted_key=None
        user_key=_create_new_user_key()
        encrypted_key=_encrypt_key(user_key,MASTER_KEY)
-       val= _save_in_db(project_id,encrypted_key)
+       val=_save_in_db(project_id,_create_cmk_id(),1,datetime.now(),encrypted_key)
+       #val=kims_db.save_in_db(project_id,encrypted_key)
        if val==0:
              encrypted_key=_check_in_db(project_id)
+             #encrypted_key=kims_db.check_in_db(project_id)
        elif val==2:
              raise Exception('Unable to write in db') 
        return encrypted_key
@@ -78,7 +81,7 @@ def _create_user_encrypted_key(project_id,MASTER_KEY):
 def _check_in_db(project_id):
        db = MySQLdb.connect("localhost","key_user","kmis_pass","kims" )
        cursor = db.cursor()
-       sql = "SELECT user_key FROM userkeys WHERE project_id ='%s'" % (project_id)
+       sql = "SELECT user_key FROM cmkkeys WHERE project_id ='%s'" % (project_id)
        result=None
        try:
           cursor.execute(sql)
@@ -93,13 +96,13 @@ def _check_in_db(project_id):
 
        return result
 
-def _save_in_db(project_id,user_key):
-       db = MySQLdb.connect("localhost","key_user","kmis_pass","kims" )
+def _save_in_db(project_id,cmk_id,version,created_at,user_key):
+       db = MySQLdb.connect("10.140.12.201","key_user","kmis_pass","kims" )
        cursor = db.cursor()
        returnval=1
-       sql = "INSERT INTO userkeys(project_id, user_key) \
-       VALUES ('%s', '%s' )" % \
-       (project_id, user_key)
+       sql = "INSERT INTO cmkkeys(project_id, cmk_id,version,created_at,user_key) \
+       VALUES ('%s', '%s','%d', '%s','%s' )" % \
+       (project_id,cmk_id,version,created_at,user_key)
        try:
           cursor.execute(sql)
           db.commit()
@@ -117,9 +120,9 @@ def _save_in_db(project_id,user_key):
        db.close()
        return returnval
 
-def _create_new_volume_encrypted_key(user_key):
+def _create_new_volume_encrypted_key(user_key,cmk_id,version):
        volume_key=_create_new_volume_key()
-       encrypted_key=_encrypt_key(volume_key,user_key)
+       encrypted_key=_encrypt_key(volume_key,user_key)+separator+cmk_id+separator+version
        return encrypted_key
 
 def _create_new_volume_key():
@@ -147,6 +150,12 @@ def _decrypte_key(key,encryption_key):
        decryption_suite = AES.new(encryption_key, AES.MODE_ECB)
        plain_text = decryption_suite.decrypt(base64.b64decode(key))
        return plain_text
+
+def _create_new_master_key():
+       return _create_new_user_key()
+
+def _create_cmk_id():
+       return _create_new_user_key()
 
 
  
